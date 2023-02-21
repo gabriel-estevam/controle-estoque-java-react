7185a5478ec99ba2689dacee904b87daaf6828c6
= <=>
2023-02-21 16:08:43 -0300

    Ajuste redirect

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,15 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { AuthProvider } from './contexts/AuthProvider';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <AuthProvider>
       <App />
-    </AuthProvider>
   </React.StrictMode>
 );
--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import { MenuLateral } from '../../components/MenuLateral';
 import { Button } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
 
 function Home() {
     console.log("token veio" + localStorage.getItem("token"))
+    const navigate = useNavigate();
+    const logout = () => {localStorage.removeItem("token"); navigate("/"); };
     return (
     <>
         <MenuLateral>
             <Button>Toogle</Button>
+            <Button onClick={logout}>Logout</Button>
         </MenuLateral>
     </>
     )
